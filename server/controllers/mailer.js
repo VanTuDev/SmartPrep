05aafd84c010118585f4d89454a563c43f95f5f9
VanTuDev <Vantu.dev@gmail.com>
2024-09-20 11:41:59 +0700

    update

--- server/controllers/mailer.js
+++ server/controllers/mailer.js
@@ -1,9 +1,7 @@
 import nodemailer from 'nodemailer';
 import Mailgen from 'mailgen';
 import ENV from '../config.js';
-
-// Cấu hình cho Nodemailer
-let nodeConfig = {
+const nodeConfig = {
     host: "smtp.ethereal.email",
     port: 587,
     secure: false, // true cho cổng 465, false cho các cổng khác
@@ -12,12 +10,8 @@ let nodeConfig = {
         pass: ENV.PASSWORD, // mật khẩu cho tài khoản Ethereal
     }
 };
-
-// Tạo transporter cho Nodemailer
-let transporter = nodemailer.createTransport(nodeConfig);
-
-// Tạo Mailgen để tạo nội dung email
-let MailGenerator = new Mailgen({
+const transporter = nodemailer.createTransport(nodeConfig);
+const MailGenerator = new Mailgen({
     theme: "default",
     product: {
         name: "Mailgen",
@@ -25,21 +19,13 @@ let MailGenerator = new Mailgen({
     }
 });
 
-/**
- * Hàm gửi email đăng ký
- * @param req - Yêu cầu từ client
- * @param res - Phản hồi đến client
- */
+
 export const registerMail = async (req, res) => {
     const { username, userEmail, text, subject } = req.body;
-
-    // Kiểm tra giá trị email
     if (!userEmail) {
         return res.status(400).send({ error: "Email is required." });
     }
-
-    // Tạo nội dung email
-    var email = {
+    const email = {
         body: {
             name: username,
             intro: text || 'Welcome to Daily Tuition! We\'re very excited to have you on board.',
@@ -47,22 +33,14 @@ export const registerMail = async (req, res) => {
         }
     };
 
-    var emailBody = MailGenerator.generate(email);
-
-    // Tạo thông điệp email
-    let message = {
+    const emailBody = MailGenerator.generate(email);
+    const message = {
         from: ENV.EMAIL,
         to: userEmail,
         subject: subject || "Signup Successful",
         html: emailBody
     };
-
-    // Gửi email
-    try {
-        await transporter.sendMail(message);
-        return res.status(200).send({ msg: "You should receive an email from us." });
-    } catch (error) {
-        console.error("Error sending email:", error); // In ra lỗi để kiểm tra
-        return res.status(500).send({ error: `Failed to send email: ${error.message}` });
-    }
+    transporter.sendMail(message).then(() => {
+        return res.status(200).send({ msg: "check box mail" })
+    }).catch(error => res.status(500).send({ error }))
 };
