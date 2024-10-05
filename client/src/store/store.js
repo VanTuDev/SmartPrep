03badbeaceeb50bdfb72ce7c55b4a8fff9fcf048
tanhtt <flywtan@gmail.com>
2024-10-05 18:01:22 +0700

    #13 add redux and update data for exam

---
+++ client/src/store/store.js
@@ -0,0 +1,10 @@
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import examReducer from './examSlice';
+
+export const store = configureStore({
+    reducer: {
+        exam: examReducer
+    },
+});
+
