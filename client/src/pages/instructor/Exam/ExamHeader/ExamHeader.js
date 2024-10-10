408d3bf6888608224bd12fa60766e209bcc71937
tanhtt <flywtan@gmail.com>
2024-10-10 08:35:07 +0700

    update crud exam

--- client/src/pages/instructor/Exam/ExamHeader/ExamHeader.js
+++ client/src/pages/instructor/Exam/ExamHeader/ExamHeader.js
@@ -4,40 +4,48 @@ import { Button, Tooltip, Tabs, Popover, TimePicker, DatePicker, message } from 
 import { X, Eye, Download, CalendarDays } from 'lucide-react';
 import dayjs from 'dayjs';
 import { useDispatch, useSelector } from 'react-redux';
-import { createExam, updateExamAPI } from 'store/examSlice';
+import { createExam, updateExamAPI, updateExam } from 'store/examSlice';
 import { useNavigate } from 'react-router-dom';
 
 const dateFormatList = 'DD/MM/YYYY';
-const format = 'HH:mm'; 
+const format = 'HH:mm';
 
 const schedulePost = (
     <div className='p-3'>
-      <div className='flex justify-center text-base font-bold'>Schedule and post content</div>
-      <div className='flex justify-between mt-3'>
-        <TimePicker size='large' className='w-1/3' defaultValue={dayjs('17:00', format)} format={format} />
-        <DatePicker size='large' defaultValue={dayjs('06/10/2024', dateFormatList)} needConfirm format={dateFormatList} />
+        <div className='flex justify-center text-base font-bold'>Schedule and post content</div>
+        <div className='flex justify-between mt-3'>
+            <TimePicker size='large' className='w-1/3' defaultValue={dayjs('17:00', format)} format={format} />
+            <DatePicker size='large' defaultValue={dayjs('06/10/2024', dateFormatList)} needConfirm format={dateFormatList} />
+        </div>
+        <div className='flex justify-end mt-2'>
+            <button className='button-normal-custom px-2 py-2' >Save</button>
+        </div>
     </div>
-    <div className='flex justify-end mt-2'>
-        <button className='button-normal-custom px-2 py-2' >Save</button>
-    </div>
-    </div>
-  );
+);
 
 function ExamHeader({ items, activeTab, onChangeTab, setExamId, examId }) {
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
     const dispatch = useDispatch();
 
     // Access the exam data from Redux (if needed)
-    const examData = useSelector((state) => state.exam);
+    const { exam, loading, error } = useSelector((state) => state.exam);
+    // const examData = useSelector((state) => state.exam);
 
-    const handleSubmit = () => {
-        // You can dispatch the Redux action here when the button is clicked
-        if(examId){
-            dispatch(updateExamAPI({ examId, examData }))
-            message.warning("Update?")
-        }else{
-            dispatch(createExam(examData));
-            message.warning("Create?");
+    const handleSubmit = (isPost) => {
+        // Update the exam status based on whether it's a post (published) or save as draft
+    const updatedExam = { ...exam, status: isPost ? 'published' : 'draft' };
+
+    // Dispatch the updateExam action to update Redux state
+    dispatch(updateExam(updatedExam)); // This updates the local state
+
+
+        // Dispatch the appropriate Redux action
+        if (examId) {
+            dispatch(updateExamAPI({ examId, examData: updatedExam }));
+            message.warning("Exam updated!");
+        } else {
+            dispatch(createExam(updatedExam));
+            message.warning("Exam created!");
         }
     };
 
@@ -79,12 +87,12 @@ function ExamHeader({ items, activeTab, onChangeTab, setExamId, examId }) {
                                     </Button>
                                 </Tooltip>
                                 <div className="flex justify-center items-center space-x-2">
-                                    <button 
-                                         onClick={handleSubmit}
-                                    className='button-outlined-custom-non-p px-2 py-2'>
+                                    <button
+                                        onClick={() => handleSubmit(false)}
+                                        className='button-outlined-custom-non-p px-2 py-2'>
                                         Save draft
                                     </button>
-                                    <button className='button-normal-custom px-2 py-2'>
+                                    <button onClick={() => handleSubmit(true)} className='button-normal-custom px-2 py-2'>
                                         Post
                                     </button>
                                     <Popover content={schedulePost} placement="bottomRight" trigger="click">
